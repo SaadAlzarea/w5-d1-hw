d4b23c7060e7e6ac1fc457ea4aab9218508fc6b7
SaadAlzarea <sfalzarea@gmail.com>
2025-05-12 06:13:32 +0300

    add price in main page

--- Products/main-page.js
+++ Products/main-page.js
@@ -3,22 +3,20 @@ fetch("https://fakestoreapi.com/products")
   .then((res) => res.json())
   .then((data) => {
     data.map((item) => {
-      //   console.log(item);
+        // console.log(item);
       let card = document.createElement("div");
       let img = document.createElement("img");
       let productName = document.createElement("p");
       let description = document.createElement("p");
       let category = document.createElement("p");
       let rating = document.createElement("p");
-      // data.rating.map((rate) => {
-      //     console.log(rate.rate);
-      // })
-      //   console.log(item.rating.rate);
+      let price = document.createElement("p");
 
       img.src = item.image;
       productName.innerHTML = item.title;
       description.innerHTML = item.description;
       category.innerHTML = item.category;
+      price.innerHTML = `${item.price}$`;
       rating.innerHTML = `${item.rating.rate} ★`;
 
       productName.classList.add("productName")
@@ -29,6 +27,7 @@ fetch("https://fakestoreapi.com/products")
       card.appendChild(productName);
       // card.appendChild(description);
       // card.appendChild(category);
+      card.appendChild(price);
       card.appendChild(rating);
       cards.appendChild(card)
 
